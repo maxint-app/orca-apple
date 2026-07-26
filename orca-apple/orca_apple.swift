cca708a1266136d81c6d5be63fea3f2ffe1ee7c5
Kingkor Roy Tirtho <krtirtho@maxint.com>
2026-07-26 12:27:28 +0600

    Initial Commit

---
+++ orca-apple/orca_apple.swift
@@ -0,0 +1,9 @@
+//
+//  orca_apple.swift
+//  orca-apple
+//
+//  Created by Kingkor Roy Tirtho on 26/7/26.
+//
+
+import Foundation
+
---
+++ orca-appleTests/orca_appleTests.swift
@@ -0,0 +1,17 @@
+//
+//  orca_appleTests.swift
+//  orca-appleTests
+//
+//  Created by Kingkor Roy Tirtho on 26/7/26.
+//
+
+import Testing
+@testable import orca_apple
+
+struct orca_appleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
