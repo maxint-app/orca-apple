781b26c3fe0c97cafd298c5bcd57f593deae9099
Kingkor Roy Tirtho <krtirtho@maxint.com>
2026-07-26 19:35:29 +0600

    chore: add swift pm declarations for example app

--- Sources/orca-example/orca_exampleApp.swift
+++ Sources/orca-example/orca_exampleApp.swift
@@ -8,7 +8,7 @@ struct OrcaExampleApp: App {
             publicKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjcm9zc3BheSIsInN1YiI6IjU0OTUwNGMwLTZlMjQtNDE2NS1hMjgxLWQyMTE4NGE0ZGNmZSIsImF1ZCI6WyJwdWJsaWMiLCJhMzhlYmMzYy0xZjFjLTRhMTgtOTFiNy1iYzZlMDIyMGZkMWMiXX0.ojYDgb25aRD68c0WgGS08XDXtrfBGM8sPaDacdZF69I",
             environment: .sandbox,
             baseURL: "http://192.168.1.101:8081",
-            enableNetworkLogging: true,
+            enableNetworkLogging: false,
             customerEmail: nil
         ))
     }
